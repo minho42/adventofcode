9820e5dd7903dafb76eb3023c7a08588521f3f27
Min ho Kim <minho42@gmail.com>
2022-12-02 00:41:29 +1100

    init

---
+++ 2022_1.py
@@ -0,0 +1,32 @@
+# https://adventofcode.com/2022/day/1
+# https://adventofcode.com/2022/day/1/input
+
+current_sum = 0
+list_of_sums = []
+
+with open("2022_1_input.txt") as file:
+    for line in file:
+        line = line.replace("\n", "")
+        if line:
+            current_sum += int(line)
+        else:
+            if current_sum > 0:
+                list_of_sums.append(current_sum)
+            current_sum = 0
+
+
+def part1():
+    print(max(list_of_sums))
+
+
+def part2():
+    top_sums = []
+    for _ in range(3):
+        temp_max = max(list_of_sums)
+        list_of_sums.remove(temp_max)
+        top_sums.append(temp_max)
+    print(sum(top_sums))
+
+
+part1()
+part2()
