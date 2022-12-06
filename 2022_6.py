492f22d217f6dddb9b4c593a80a12cba025f8b70
Min ho Kim <minho42@gmail.com>
2022-12-07 02:13:15 +1100

    add day6

---
+++ 2022_6.py
@@ -0,0 +1,18 @@
+from utils import read_input
+
+d = read_input("2022_6.in")[0]
+
+
+def p(n):
+    for i in range(len(d)):
+        try:
+            s = set([d[i + x] for x in range(n)])
+            if len(s) == n:
+                print(i + n)
+                break
+        except IndexError:
+            break
+
+
+p(4)
+p(14)
