6f49dd845ab86e39f05b0284e4cf5ba2fba5993d
Min ho Kim <minho42@gmail.com>
2022-12-24 14:44:33 +1100

    part2 🧮

--- 2022/21.py
+++ 2022/21.py
@@ -1,5 +1,6 @@
 import re
-
+from sympy import Symbol
+from sympy.solvers import solve
 from utils import read_input
 
 lines = read_input("21.in")
@@ -15,21 +16,28 @@ for line in lines:
 def f(x):
     if d[x].isdigit():
         return d[x]
-    return eval(f"( {f(d[x][:4])} ) {d[x][4]} ( {f(d[x][5:])} )")
+    return f"( {f(d[x][:4])} ) {d[x][4]} ( {f(d[x][5:])} )"
 
 
 p1 = f("root")
+p1 = eval(p1)
 p1 = int(p1)
 print(p1)
 
 
 def f2(x):
+    if "humn" in x:
+        return x
     if d[x].isdigit():
         return d[x]
     m = re.search(r"(\w+)([=+\-*\/]{1,})(\w+)", d[x])
-    return eval(f"( {f2(m[1])} ) {m[2]} ( {f2(m[3])} )")
+    return f"({f2(m[1])}) {m[2]} ({f2(m[3])})"
 
 
-d["root"] = d["root"].replace("+", "==")
+# root = x + humn
+# x = root - humn
+d["root"] = d["root"].replace("+", "-")
+d["humn"] = "humn"
 p2 = f2("root")
+p2 = solve(p2)[0]
 print(p2)
